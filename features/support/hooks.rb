10f866feb27acbf1aa3322bbe42d593396ef7d56
rafaelmdsa <rafael.maciel@bowe.co>
2021-05-03 11:10:12 -0300

    cucumber reports

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,5 +5,6 @@ end
 
 After do |scenario|
   screenshot = page.save_screenshot("logs/screenshots/#{scenario.__id__}.png")
-  embed(screenshot, "image/png", "Screenshot")
+  shot_b64 = Base64.encode64(File.open(screenshot, "rb").read)
+  embed(shot_b64, "image/png", "Screenshot")
 end
